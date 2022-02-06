6c6d30292599aae5aa7fb5b5809651c8d29b2849
shaik zuhair ahamed <zuhairahamed3114@gmail.com>
2022-02-06 12:04:12 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleanArchitectureManishNarayan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
